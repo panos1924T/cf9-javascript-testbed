6e9a57a16a6b47b2c32fc412b6f09b60f143f02b
PanTs <pants.ath@gmail.com>
2026-02-26 17:15:31 +0200

    feat: add hello method and run

--- ch1-excersices/objectUser.js
+++ ch1-excersices/objectUser.js
@@ -2,10 +2,15 @@
 const user = {
     name: `PanTs`,
     age: `26`,
-    city: `Athens`
+    city: `Athens`,
+
+    hello() {
+    console.log(`Hello ${this.name}`);
+    }
 };
 
 console.log(`The user `, user.name, " is ", user.age, " years old and lives in ", user.city);
 
 console.log(JSON.stringify(user));
 
+user.hello();
