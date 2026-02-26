cc239e3a77ec4741da876664f6d9eb5a2fe95f04
PanTs <pants.ath@gmail.com>
2026-02-26 16:43:14 +0200

    feat: add push an array and print with for each

---
+++ ch1-excersices/array.js
@@ -0,0 +1,9 @@
+const cities = [`Athens` , `London`, `Paris`];
+
+// for (const city of cities) console.log(city);
+
+// cities[3] = `Berlin`;
+
+cities.push(`Berlin`);
+
+for (const city of cities) console.log(city);
